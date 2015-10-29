bcd50dcd23c4a3b53744b469551c760482e7408b
hourtkeak <hourtkeak@yahoo.com>
2015-10-29 23:10:28 +0700

    commit

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -13,7 +13,37 @@
 <div id="data-projecct">
 	<div class="container">
 		<div class="row">
-	        {!!$menu->content!!}
+	      	<div class="col-md-3">
+	      		<div style="text-align:center;">
+
+				<a class="fancybox" href="{{ asset('/images/about-company-logo.png')}}" data-fancybox-group="gallery" >
+	      				<img src="{{ asset('/images/about-company-logo-thumbnail.png')}}">
+	      		</a>
+	      		<h3 style="color:#666; font-size:16px;"> Company Logo</h3>
+	      		</div>
+
+
+	      	</div>
+	      	<div class="col-md-9">
+	        		{!!$menu->content!!}
+	        		<p style="font-size:16px; line-height:24px; text-align:justify;">Green Global Architecture Design & Construction Co.,Ltd Company repaired by Mr Ry Thyra. Mr Ry Thyra worked at BOREY LAYKONG in two years go with working and studying in one university in Cambodia. Than Mr Ry Thyra has studied more on object at Vietnam country wile 7 months of finishing on this object he came back to Cambodia. Mr Ry Thyra has worked in company part of Architecture & Design in these he got experience like Exterior Design, Interior Design, Furniture Design, Design electric system and Design water system. Mr Ry Thyra has good experience and focus on work and on time on work. Mr Ry Thyra has experience for meeting customer and partner very good also. Than Mr Ry Thyra has cooperation with partner 4 people. The time in cooperation with Mr Ry Thyra and members partners make this company name GAC-4 on there time Mr Ry Thyra has cooperation 3 months. Mr Ry Thyra make one company like title on up line in 3 months already. All of these like: </p>
+						<ul style="list-style: disc; font-size:16px; line-height:24px;">
+							<li>Architecture Design</li>
+							<li>Exterior Decoration</li> 
+							<li>Interior Decoration</li>
+							<li>Furniture Design</li>
+							<li>M & E water supply</li>
+							<li>Construction & Management</li> 
+							<li>Construction Contractor</li>
+						</ul>
+						<p style="font-size:16px; line-height:24px; text-align:justify;">
+						* Location of company: #70, Phreak Shihanouk Bvl, Sangkat Tunle Basak, Khan chhomkamorn, Phnom Penh, Cambodia.<br>
+						Website: www.green-globale.com<br>
+						Email: Info@green-globale.com<br>
+						Phone number: (+855) 93 411676 / 78 900 676</p>
+
+	        		</p>
+	        </div>
 	    </div>
 	</div>
 </div>
