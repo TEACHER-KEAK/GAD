0394576b398d49c55491b21a0ad772c478f9015f
hourtkeak <hourtkeak@yahoo.com>
2015-11-10 14:13:24 +0700

    resize

--- app/Http/Controllers/Admin/ImageController.php
+++ app/Http/Controllers/Admin/ImageController.php
@@ -112,7 +112,7 @@ class ImageController extends Controller
             $image->move('images/source/', $filename);
             
             $thumb = Image::make('images/source/'.$filename)
-                          ->resize(320,213)
+                          ->resize(500, 338)
                           ->save('images/thumbs/'.$filename,50);
             
         }
