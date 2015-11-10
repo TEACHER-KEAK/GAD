bdf1f7ae9043a4e4adbaef374369138c4e356e31
hourtkeak <hourtkeak@yahoo.com>
2015-11-10 13:26:30 +0700

    change lan

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -83,7 +83,8 @@ type='text/css'>
     <!--[if lt IE 7]>
     <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
     <![endif]-->
-    <!--ads popup-->        
+    <!--ads popup-->   
+         
     <div id="popup">
         <span class="button b-close"><span>X</span></span>
             <a href="#"><img src="{{ asset('/images/pop_ads.jpg')}}" /></a>
